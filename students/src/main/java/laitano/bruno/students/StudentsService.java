58d337f84d46ba650d85c3d194aaeae3f39d9d21
Bruno Laitano <bruno.grigoletti.laitano@gmail.com>
2024-10-28 21:42:26 -0300

    All microservices packaged successfully, and docker files created

--- students/src/main/java/laitano/bruno/students/StudentsService.java
+++ students/src/main/java/laitano/bruno/students/StudentsService.java
@@ -2,10 +2,11 @@ package laitano.bruno.students;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan (basePackages = {"laitano.bruno.students","laitano.bruno.entities"})
 public class StudentsService {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsService.class, args);
 	}
--- subjects/src/main/java/laitano/bruno/subjects/SubjectsService.java
+++ subjects/src/main/java/laitano/bruno/subjects/SubjectsService.java
@@ -2,10 +2,12 @@ package laitano.bruno.subjects;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-public class SubjectsService {
+@EntityScan (basePackages = {"laitano.bruno.subjects","laitano.bruno.entities"})
 
+public class SubjectsService {
 	public static void main(String[] args) {
 		SpringApplication.run(SubjectsService.class, args);
 	}
